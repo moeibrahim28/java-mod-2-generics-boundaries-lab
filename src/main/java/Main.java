26de711eb3ac8b6d7ba0bab1dd2ffce9ac7af30a
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-28 16:03:44 -0400

    implemented averageList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     // implement the public averageList method here
-    
+
     public static void main(String[] args) {
         List<Integer> intList = List.of(1, 2, 3, 4, 5);
         System.out.println(averageList(intList)); // 3.0
 
         List<Double> doubleList = List.of(1.0, 2.0, 3.0, 4.0, 5.0);
-        System.out.println(averageList(doubleList)); // 3.0       
+        System.out.println(averageList(doubleList)); // 3.0
     }
+
+    public static double averageList(List<? extends Number> list){
+        double sum=0.0;
+        double counter=0.0;
+        for (Number number : list) {
+            sum+=number.doubleValue();
+            counter++;
+        }
+        return sum/counter;
+
+    }
+
+    
 }
